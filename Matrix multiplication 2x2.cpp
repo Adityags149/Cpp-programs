28e8c7eb9b064e6e70f872068f3d0dd8fb74856a
ADITYA GUSAIN <104920194+Adityags149@users.noreply.github.com>
2023-10-01 17:19:25 +0530

    Update Matrix multiplication 2x2.cpp

--- Matrix multiplication 2x2.cpp
+++ Matrix multiplication 2x2.cpp
@@ -2,13 +2,21 @@
 using namespace std;
 int main()
 {
-    int i, j, A[2][2], B[2][2];
+    int i, j, A[2][2], B[2][2], k, mul[2][2];
     cout << "Enter Matix 1: " << endl;
     for (i = 0; i < 2; i++)
     {
         for (j = 0; j < 2; j++)
         {
             cin >> A[i][j];
+        }
+    }
+    cout << "Enter Matix 2: " << endl;
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            cin >> B[i][j];
         }
     }
     cout << "Matix 1: " << endl;
@@ -20,14 +28,6 @@ int main()
         }
         cout << endl;
     }
-    cout << "Enter Matix 2: " << endl;
-    for (i = 0; i < 2; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            cin >> B[i][j];
-        }
-    }
     cout << "Matix 2: " << endl;
     for (i = 0; i < 2; i++)
     {
@@ -37,12 +37,23 @@ int main()
         }
         cout << endl;
     }
-    cout << "Multiplied Matix is: " << endl;
+    cout << "multiplied matrix" << endl;
     for (i = 0; i < 2; i++)
     {
         for (j = 0; j < 2; j++)
         {
-            cout << A[i][j] + B[i][j] << " ";
+            mul[i][j] = 0;
+            for (k = 0; k < 2; k++)
+            {
+                mul[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            cout << mul[i][j] << " ";
         }
         cout << endl;
     }
